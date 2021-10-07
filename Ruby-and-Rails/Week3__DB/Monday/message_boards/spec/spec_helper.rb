81fc3b7c07aebdbf0fe32021929830b83a36c5db
Alek <alekseii.sm@gmail.com>
2021-10-08 00:01:19 +0300

    add `universal` spec helper

---
+++ Ruby-and-Rails/Week3__DB/Monday/message_boards/spec/spec_helper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require 'pg'
+require 'pry'
+require './app'
+require 'capybara/rspec'
+require 'sinatra'
+
+require 'board'
+require 'message'
+
+DB = PG.connect(dbname: 'message_boards_test')
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec('DELETE FROM boards *;')
+    DB.exec('DELETE FROM messages *;')
+  end
+end
