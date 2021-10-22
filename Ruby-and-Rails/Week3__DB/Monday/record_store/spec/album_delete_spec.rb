672bd555cc96b8d65c1f33221ef45d7cd3e5c314
Alek <alekseii.sm@gmail.com>
2021-10-22 06:01:16 +0300

    optimize Album#delete test

--- Ruby-and-Rails/Week3__DB/Monday/record_store/spec/album_delete_spec.rb
+++ Ruby-and-Rails/Week3__DB/Monday/record_store/spec/album_delete_spec.rb
@@ -3,12 +3,23 @@
 require_relative './spec_helper.rb'
 
 describe('#delete') do
+  before :each do
+    @album = Album.new(name: 'A Love Supreme', id: nil)
+    @album.save
+    @song = Song.new(name: 'Naima', album_id: @album.id, id: nil)
+    @song.save
+    @artist_name = 'Kooshkineen Uurve'
+    @artist = Artist.new(@artist_name)
+    @artist.update(album_name: @album.name)
+
+    @album.delete
+  end
+
   it('deletes all songs belonging to a deleted album') do
-    album = Album.new(name: 'A Love Supreme', id: nil)
-    album.save
-    song = Song.new(name: 'Naima', album_id: album.id, id: nil)
-    song.save
-    album.delete
-    expect(Song.find(song.id)).to(eq(nil))
+    expect(Song.find(@song.id)).to eq(nil)
+  end
+
+  it 'deletes all association with artists' do
+
   end
 end
