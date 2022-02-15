a007f4005db403b013fe6067b32c5c3bf3cc55cb
Alek <alekseii.sm@gmail.com>
2022-02-15 03:57:09 +0200

    add delete sight feature

--- Ruby-and-Rails/Week7__Rails_ActiveRecords/wildlife_tracker/app/controllers/sights_controller.rb
+++ Ruby-and-Rails/Week7__Rails_ActiveRecords/wildlife_tracker/app/controllers/sights_controller.rb
@@ -7,16 +7,24 @@ class SightsController < ApplicationController
   end
 
   def create
-    @animal = Animal.find(params[:animal_id])
+    animal = Animal.find(params[:animal_id])
     # tests work w/o this:
     # location = params[:sight][:location].split(',').map(&:to_f)
-    @sight = @animal.sights.new(sight_params)
+    sight = animal.sights.new(sight_params)
 
-    if @sight.save
-      redirect_to animal_path(@animal)
+    if sight.save
+      redirect_to animal_path(animal)
     else
       render :new
     end
+  end
+
+  def destroy
+    animal = Animal.find(params[:animal_id])
+    sight = Sight.find(params[:id])
+    sight.destroy
+
+    redirect_to animal_path(animal)
   end
 
   private
--- Ruby-and-Rails/Week7__Rails_ActiveRecords/wildlife_tracker/spec/views/animal_show_spec.rb
+++ Ruby-and-Rails/Week7__Rails_ActiveRecords/wildlife_tracker/spec/views/animal_show_spec.rb
@@ -1,20 +1,35 @@
 require 'rails_helper'
 
 describe('Show animal', type: :feature) do
-  it 'shows animal species' do
+  before :each do
     Animal.destroy_all
-    animal = Animal.create(species: 'Vydra')
-    locations = (1..10).map(&:to_f).zip((101..110).map(&:to_f))
-    locations.each { |location| animal.sights.create(location: location) }
+    @animal = Animal.create(species: 'Vydra')
+    @locations = (1..10).map(&:to_f).zip((101..110).map(&:to_f))
+    @locations.each { |location| @animal.sights.create(location: location) }
+  end
 
-    visit "/animals/#{animal.id}"
+  it 'shows animal species and sights' do
+    visit "/animals/#{@animal.id}"
+    expect(page).to have_http_status(:success)
 
-    expect(page.status_code).to eq 200
     expect(find('h1').text).to eq 'Vydra'
 
-    expect(all('#location').map(&:text)).to match_array(locations.map { |x, y| "#{x}, #{y}" })
- 
-    expect(page).to have_link('Edit', href: "/animals/#{animal.id}/edit")
-    expect(page).to have_link('Delete', href: "/animals/#{animal.id}")
+    expect(all('#location').map(&:text)).to match_array(@locations.map { |x, y| "#{x}, #{y}" })
+
+    expect(page).to have_link('Edit', href: "/animals/#{@animal.id}/edit")
+    expect(page).to have_link('Delete', href: "/animals/#{@animal.id}")
+  end
+
+  it 'allows to delete a sight' do
+    visit "/animals/#{@animal.id}"
+    expect(page).to have_http_status(:success)
+
+    within(find('#sight', text: '5.0, 105.0')) do
+      click_on 'Delete'
+    end
+    expect(page).to have_http_status(:success)
+
+    @locations.delete([5, 105])
+    expect(all('#location').map(&:text)).to match_array(@locations.map { |x, y| "#{x}, #{y}" })
   end
 end
