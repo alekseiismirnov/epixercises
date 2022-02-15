e1cd21b2e20705d9f83203ac088a653f7905d28e
Alek <alekseii.sm@gmail.com>
2022-02-15 02:27:40 +0200

    show sight coordinates

--- Ruby-and-Rails/Week7__Rails_ActiveRecords/wildlife_tracker/spec/views/animal_show_spec.rb
+++ Ruby-and-Rails/Week7__Rails_ActiveRecords/wildlife_tracker/spec/views/animal_show_spec.rb
@@ -4,11 +4,16 @@ describe('Show animal', type: :feature) do
   it 'shows animal species' do
     Animal.destroy_all
     animal = Animal.create(species: 'Vydra')
+    locations = (1..10).map(&:to_f).zip((101..110).map(&:to_f))
+    locations.each { |location| animal.sights.create(location: location) }
+
     visit "/animals/#{animal.id}"
 
     expect(page.status_code).to eq 200
     expect(find('h1').text).to eq 'Vydra'
 
+    expect(all('div').map(&:text)).to match_array(locations.map { |x, y| "#{x}, #{y}" })
+ 
     expect(page).to have_link('Edit', href: "/animals/#{animal.id}/edit")
     expect(page).to have_link('Delete', href: "/animals/#{animal.id}")
   end
