cd3a7ea893de8f6f82edd8eee356e25170fa5939
Alek <alekseii.sm@gmail.com>
2022-07-17 22:24:54 +0300

    add edit division feature

--- Ruby-and-Rails/Week7__Rails_ActiveRecords/employee_tracker/app/controllers/divisions_controller.rb
+++ Ruby-and-Rails/Week7__Rails_ActiveRecords/employee_tracker/app/controllers/divisions_controller.rb
@@ -1,7 +1,26 @@
 class DivisionsController < ApplicationController
   def index
     @divisions = Division.all
-    
     render :index
+  end
+
+  def edit
+    @division = Division.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @division = Division.find(params[:id])
+    if @division.update(division_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def division_params
+    params.require(:division).permit(:name)
   end
 end
--- Ruby-and-Rails/Week7__Rails_ActiveRecords/employee_tracker/spec/features/division_list_page_spec.rb
+++ Ruby-and-Rails/Week7__Rails_ActiveRecords/employee_tracker/spec/features/division_list_page_spec.rb
@@ -4,13 +4,30 @@ require 'joy_division_helper'
 feature 'Division CRUD' do
   before :all do
     @company = JoyDivision.new
+    @name_to_edit = 'ICQ'
+    @correct_name = 'Jabber'
   end
 
   scenario 'HR Manager lists divisions' do
     visit divisions_path
     expect(page).to have_http_status(:success)
     within('.divisions') do
-      expect(all('.divisions__division').map(&:text)).to match_array @company.divisions_names
+      expect(all('.divisions__division_link').map(&:text)).to match_array @company.divisions_names
     end
+  end
+
+  scenario 'HR Manager updates division name' do
+    visit divisions_path
+    within(find('.divisions__division', text: @name_to_edit)) do
+      click_on 'Edit'
+    end
+
+    expect(page).to have_http_status(:success)
+    fill_in 'division_name',	with: @correct_name
+    click_on 'Submit'
+
+    expect(page).to have_http_status(:success)
+    expect(page).to have_no_text(@name_to_edit)
+    expect(page).to have_text(@correct_name)
   end
 end
